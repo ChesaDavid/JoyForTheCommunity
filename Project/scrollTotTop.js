95ca0b5c9cda264973123d783cb460dcaed8b34c
Cheșa David <140747134+ChesaDavid@users.noreply.github.com>
2024-02-22 21:55:10 +0200

    A new commit ,added Cntact,People folders with their html and css files respectavly

--- Project/scrollTotTop.js
+++ Project/scrollTotTop.js
@@ -3,14 +3,28 @@ let button = document.getElementById("go-to-the-top");
 window.onscroll = function() {scrollFunction()};
 
 function ScrollFunction(){
-    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
-        button.style.display = "block";
-    }
-    else{
-        button.style.display = "none";
-    }
+    indow.scrollY > window.innerHeight
+    ? button.classList.add("show")
+    : button.classList.remove("show");
 }
+
+
 function toFunction(){
-  document.body.scrollTop = 0;
+  document.body.scrollTop = 0 , "slow";
   document.documentElement.scrollTop = 0
+}
+let moreButton = document.getElementById("more-button");
+var isPressed = 0;
+if(moreButton.onclick()){
+    switch(isPressed){
+        case 0:
+            isPressed = 1;
+            document.getElementById("unseeanList").style.display = "block";
+            break;
+        case 1:
+            isPressed = 0;
+            document.getElementById("unseeanList").style.display = "none";
+            break;
+    }
+
 }
